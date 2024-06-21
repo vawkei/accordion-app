0fcfd059285efc04057f797eb2180cdbc31eb0e3
voke sokoh <vokes9810@gmail.com>
2024-06-21 06:35:07 +0100

    added a kinda title in the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-//No external help from the internet, no google,no chatgpt, no nothing. Just my brain.
+//This is partOne of the code-challenges series:Accordion app. 
+//No external help from the internet, no google,no chatgpt, no nothing. Just my brain. Let's go
 
 import Home from "./components/home/Home";
 import Layout from "./components/layout/Layout";
